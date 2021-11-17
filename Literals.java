cb6800c11c65a692642d48b78ac39a0d8b40b1ee
Mohamed ELmursi <elmu0006@algonquinlive.com>
2021-11-16 22:26:08 -0500

    Literals

---
+++ Literals.java
@@ -0,0 +1,19 @@
+public class Literals {
+    public static void main(String[] args) {
+        boolean test = true;
+        System.out.println(test);
+
+        double rate  = 1.5;
+        System.out.println(rate);
+
+        test = rate > 1.0;
+        boolean test2 = rate < 1.0;
+        System.out.println(test);
+        System.out.println(test2);
+
+
+
+
+    }
+
+  }
